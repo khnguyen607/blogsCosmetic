2d7c0bf2cd7240bf1981542026789faeed0b0213
khnguyen607 <khnguyen.job@gmail.com>
2024-05-15 14:12:56 +0700

    upload..

---
+++ backend/Models/BlogModel.php
@@ -0,0 +1,65 @@
+<?php
+
+class BlogModel extends BaseModel
+{
+    const TABLE = 'blogs';
+
+    public function mAlls($select = ['*'])
+    {
+        return $this->bmAlls(self::TABLE, $select);
+    }
+
+    public function mFind($id)
+    {
+        return $this->bmFind(self::TABLE, $id);
+    }
+
+    public function mInsert($data)
+    {
+        return $this->bmInsert(self::TABLE, $data);
+    }
+
+    public function mUpdate($id, $data)
+    {
+        return $this->bmUpdate(self::TABLE, $id, $data);
+    }
+
+    public function mDelete($id)
+    {
+        return $this->bmDelete(self::TABLE, $id);
+    }
+
+    public function mGetComments($id)
+    {
+        $sql = "SELECT comments.*, users.Name AS userName
+                FROM `comments` 
+                INNER JOIN users ON comments.userID = users.ID
+                WHERE comments.productID=$id";
+        $query = $this->_query($sql);
+        $data = [];
+
+        while ($row = mysqli_fetch_assoc($query)) {
+            array_push($data, $row);
+        }
+
+        return $data;
+    }
+
+    public function mGetGenres($id)
+    {
+        $sql = "SELECT genres.*
+                FROM `genres`
+                INNER JOIN syn_movies_genres ON genres.ID = syn_movies_genres.genreID
+                INNER JOIN movies ON syn_movies_genres.movieID = movies.ID
+                WHERE syn_movies_genres.movieID = $id";
+        $query = $this->_query($sql);
+        $data = [];
+
+        while ($row = mysqli_fetch_assoc($query)) {
+            array_push($data, $row);
+        }
+
+        return $data;
+    }
+
+}
---
+++ frontend/pages/blogs/blogs.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    // await Helper.fetchBackendLink();
+    // Đổ danh sách sản phẩm
+    _init();
+})
+
+async function _init() {
+    var items = await Helper.fetchData("blog")
+    showDatas(items)
+    // Thêm sự kiện lọc giá khi bấm lọc
+    // document.querySelector(".price-range-area .text-uppercase").addEventListener('click', () => filterBy())
+    // document.querySelector(".shop_sidebar_searchbar i").addEventListener('click', () => filterBy())
+}
+
+async function filterBy() {
+    document.querySelectorAll("#grid .col-lg-3").forEach(item => {
+        if (
+            byName(item.querySelector(".product-name a").textContent)
+        ) {
+            item.classList.remove("d-none")
+        } else {
+            item.classList.add("d-none")
+        }
+    })
+
+    // Lọc theo tên
+    function byName(name) {
+        var nameValue = Helper.getParameterByName('q')
+        if (name.toLowerCase().includes(nameValue.toLowerCase())) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+}
+
+async function showDatas(items) {
+    var dataList = document.querySelector(".dataList")
+    var dataItem = dataList.querySelector(".dataItem").cloneNode(true)
+    dataList.innerHTML = ""
+    await items.forEach(item => {
+        let cloneData = dataItem.cloneNode(true)
+
+        cloneData.querySelector(".title a").textContent = item.Title
+        cloneData.querySelectorAll("a[href='./?page=blogDetails']").forEach(i=>i.href=`./?page=blogDetails&id=${item.ID}`)
+        cloneData.querySelector("img").src = Helper.getLink(item.Img)
+        cloneData.querySelector("p").textContent = item.Subtitle
+
+        const date = new Date(item.Date);
+        cloneData.querySelector(".date").textContent = date.getDate()
+        cloneData.querySelector(".month").textContent = "Th"+(date.getMonth() + 1)
+        
+        // cloneData.querySelector(".rate span").textContent = item.Rate
+        // cloneData.querySelector(".hvr-inner a").href = `./?page=movieDetails&id=${item.ID}`
+
+        dataList.appendChild(cloneData)
+    });
+    // document.querySelector(".shop_sidebar_searchbar input").value = Helper.getParameterByName("search")
+    filterBy()
+}
